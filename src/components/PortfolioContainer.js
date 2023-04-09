2dfbdb31a729722818d9d2b5e538cc938f05dae2
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-04-09 02:50:30 -0500

    adding tab bars

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import NavTabs from './Navtabs';
-import About from '.';
-import Resume from '.';
-import Contact from './t';
-import Portfolio from './';
+import About from './pages/AboutMe';
+import Resume from './pages/MyResume';
+import Contact from './pages/ContactMe';
+import Portfolio from './pages/MyPortfolio';
 
 export default function PortfolioContainer() {
     const [currentPage, setCurrentPage] = useState('Home');
@@ -11,25 +11,27 @@ export default function PortfolioContainer() {
     // TODO: Add a comment describing the functionality of this method
     //  checking to see if currentpage is one of the nav links and then render page if it is
     const renderPage = () => {
-      if (currentPage === 'About') { //about me
-        return < />;
+      if (currentPage === 'About') {
+        return <About />;
       }
-      if (currentPage === '') { // my portfolio
-        return < />;
+      if (currentPage === 'Portfolio') {
+        return <Portfolio />;
       }
-      if (currentPage === 'Resume') { // my resume
-        return </>;
+      if (currentPage === 'Resume') {
+        return <Resume />;
       }
-      return < />;
+      return <Contact />;
     };
 
     const handlePageChange = (page) => setCurrentPage(page);
 
     return (
       <div>
-        {}
+        {/* // TODO: Add a comment describing what we are passing as props 
+                  the props are that are being passed in are handlepage change and currentpage*/}
         <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-        {}
+        {/* // TODO: Add a comment explaining what is happening on the following line 
+                redndering the data from the props*/}
         {renderPage()}
       </div>
     );
