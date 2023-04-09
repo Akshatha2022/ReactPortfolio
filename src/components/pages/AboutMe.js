32f0547c501188f36bc574afb294b2836d557bde
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-04-09 03:02:17 -0500

    about me page

---
+++ src/components/pages/AboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+     <p>
+This is about Me. I want to be a Full Stack Developer. 
+      </p>
+    </div>
+  );
+}
