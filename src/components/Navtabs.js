a0bdddab299ae6435196bcee5220a72e3e610c8e
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-04-09 02:37:36 -0500

    experiment navbars

--- src/components/Navtabs.js
+++ src/components/Navtabs.js
@@ -1,46 +1,21 @@
-import React, { useState } from 'react';
-import BucketForm from './PortfolioContainer';
+import React from 'react';
 
-function Bucket(props) {
-  const [edit, setEdit] = useState({
-    id: null,
-    value: '',
-    eagerness: '',
-  });
-
-  console.log(props.bucket);
-
-  const submitUpdate = (value) => {
-
-    // TODO: Write logic to update the `edit` value in state after a user updates an entry in the list
-
-    // TODO: Set the key:value pairs in the `edit` object back to empty strings
-
-  };
-
-  // If the user is attempting to edit an item, render the bucket form with the edit variable passed as a prop
-  if (edit.id) {
-    return <BucketForm edit={edit} onSubmit={submitUpdate} />;
-  }
-
-  return props.bucket.map((item, index) => (
-    // TODO: Add a className of `bucket row complete ${item.eagerness}` for completed items, and `bucket-row ${item.eagerness}` for non-completed items
-    // TODO: Add a key attribute set to the value of the index position
-    // Hint: use a ternary operator
-    <div className={ } key={}>
-
-      // TODO: Add an onClick event that invokes the `completeBucketItem` method passing the item id as a argument
-      <div key={} onClick={}>
-          {/* TODO: Add the item text here */}
-      </div>
-      <div className="icons">
-        // TODO: Add an onClick event update the `edit` object with the `id`, `value`, and `eagerness` properties
-        <p onClick={}> ✏️</p>
-        {/* TODO: Add an onClick event that will invoke the removeBucketItem method passing in the `item.id` */}
-        <p onClick={}> 🗑️</p>
-      </div>
-    </div>
-  ));
-}
-
-export default Bucket;
+// TODO: Add a comment explaining how we are able to extract the key value pairs from props
+//   currentPage and handlePageChange are passed in as props 
+// deconstructing the props object  
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          //  TODO: Add a comment explaining what this logic is doing
+            // ternary operator is checking logic is home nav link is active 
+          className={currentPage === 'About Me' ? 'nav-link active' : 'nav-link'}
+        >
+          About Akshatha
+        </a>
+      </li>
+      
+export default NavTabs;
