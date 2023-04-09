be6d862496b509e1005d81af7ed9d9f3e6990dba
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-04-09 00:49:17 -0500

    components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import PortfolioContainer from './components/PortfolioContainer';
-import Footer from './components/Footer';
-import { Form } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <PortfolioContainer/>
-      <Footer/>
-      <Form/>
     </div>
   );
 }
