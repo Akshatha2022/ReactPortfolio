de27f2eacef4397978e9fd6497dcaf7afb65bee7
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-04-09 02:39:12 -0500

    navtabs complete

--- src/components/Navtabs.js
+++ src/components/Navtabs.js
@@ -18,4 +18,43 @@ function NavTabs({ currentPage, handlePageChange }) {
         </a>
       </li>
       
+      <li className="nav-item">
+        <a
+          href="#portfolio"
+          onClick={() => handlePageChange('Portfolio')}
+          //  TODO: Add a comment explaining what this logic is doing
+            // ternary operator is checking logic is home nav link is active 
+          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+        >
+          Portfolio
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#resume"
+          onClick={() => handlePageChange('Resume')}
+          //  TODO: Add a comment explaining what this logic is doing
+                // ternary operator is checking if the currentPage is equal to blog 
+                // if its is the it will return to blog 
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          My Resume
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          //  TODO: Add a comment explaining what this logic is doing
+           // if current page is contact then return the page contact  
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact Me
+          
+        </a>
+      </li>
+    </ul>
+  );
+}
+
 export default NavTabs;
