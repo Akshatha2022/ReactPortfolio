8518cf4e6202c3d1f47fb8c597dbc2a6798d6494
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-04-10 14:32:29 -0500

    setting up footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,22 +5,66 @@ const styles = {
         display: 'flex',
       marginTop: 1000,
       background: 'light gray',
-    },
-    content: {
-        display: 'flex',
-      padding: 20,
-      marginTop: '1000'
+      height: 100,
+      textAlign: 'center',
+      marginBottom: '4rem'
     },
   };
 
   function Footer() {
     return (
       <div style={styles.footer}>
-         <div style={styles.content}>
-            'creating a footer'
+         
+         <footer className="footer">
+			<div>
+				<a
+					href="https://github.com/Akshatha2022/ReactPortfolio"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("../assets/github-logo.webp")}
+						alt="Github"
+						className="logo"
+					></img>
+				</a>
+			</div>
+            <div>
+				<a
+					href="https://twitter.com/akshatha_bk"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("../assets/twitter logo.jpg")}
+						alt="Twitter"
+						className="logo"
+					></img>
+				</a>
+			</div>
+
+            <div>
+				<a
+					href=""
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("../assets/Linkedin logo.png")}
+						alt="Linkedin"
+						className="logo"
+					></img>
+				</a>
+			</div>
+
+
+
+            </footer>
             </div>
-      </div>
+           
+    
     );
   }
+  
 
 export default Footer;
